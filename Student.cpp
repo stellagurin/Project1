95b9be82e02e9eec0b94b27bda95df503e5d937a
stellagurin <stellagurin@gmail.com>
2021-03-19 12:42:47 -0400

    Added Student.cpp

---
+++ Student.cpp
@@ -0,0 +1,39 @@
+
+#include<string>
+
+using namespace std;
+
+#include <iostream>
+#include "Student.h"
+
+
+
+Student::Student(){
+    studentID = 0;
+    studentName = "";
+}
+
+Student::Student(int id, string name){
+  studentID = id;
+  studentName = name;
+}
+
+void Student::setStudentId(int id){
+  studentID = id;
+}
+
+void Student::setStudentName(string name){
+  studentName = name;
+}
+
+int Student::getStudentId(){
+  return studentID;
+}
+
+string Student::getStudentName(){
+  return studentName;
+}
+
+bool Student::operator==(const Student &other) const{
+  return (studentID == other.studentID);
+}
